e176b60df2c6f2fb31813d7625d418cbcc51e379
govin698438 <govin698438@cdh.com.tw>
2021-08-08 17:16:25 +0800

    a

---
+++ FCP/Core/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FCP.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ FCP/MVVM/Models/Enum/ConvertResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCP.MVVM.Models.Enum
+{
+    public enum ConvertResult
+    {
+        成功,
+        失敗,
+        全數過濾,
+        沒有頻率
+    }
+}
